6c8b5ff3333edbf17f5764a6201598cd358bb785
y-shimomi <80519864+y-shimomi@users.noreply.github.com>
2021-04-17 18:39:58 +0900

    Add files via upload

---
+++ ex03-3.js
@@ -0,0 +1,11 @@
+const array = [1, 2, 3]
+
+console.log(array[1], array[2])
+
+console.log(array[1], array[2], array[1] + array[2])
+
+console.log(array[1])
+
+
+array.push(100)
+console.log(array[3])
---
+++ main.js
@@ -0,0 +1,12 @@
+/* console.log('Hello,World!')
+console.log('こんにちは、世界！')
+console.log(1 + 2)
+console.log(10 - 4 * 8) */
+
+const name = "Taro Yamada"
+const isValid = true
+const userIds = [1, 3, 10]
+
+console.log(name)
+console.log(isValid)
+console.log(userIds)
